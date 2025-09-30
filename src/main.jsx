a5485eea3ef2ba9b7900beca0d6f1a1f52718fde
7t4o <hatemlaptop1@gmail.com>
2025-09-30 16:14:28 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,5 @@ createRoot(document.getElementById('root')).render(
 )
 
 if ("serviceWorker" in navigator) {
-  const base = import.meta.env.BASE_URL || "/";
-  navigator.serviceWorker.register(`${base}sw.js`);
+  navigator.serviceWorker.register(`sw.js`);
 }
